887c136fd0e09f80054b04ed62a417905da4b17b
Albert <playm8419@gmail.com>
2026-05-06 23:57:52 +0700

    Add files via upload

---
+++ Latihan Informatika/Informatika/Tugas Informatika Pertemuan 7/main.js
@@ -0,0 +1,14 @@
+function calc(a, b, op){
+    while(op == "tambah"){
+        return a+b ;
+    }
+    while(op == "kurang"){
+        return a-b ;
+    }
+    while(op == "kali"){
+        return a*b ;
+    }
+    while(op == "bagi"){
+        return a/b ;
+    }
+}
